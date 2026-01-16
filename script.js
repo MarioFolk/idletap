d9c96f22720380d35829bc8267d4943d13ba2256
Mario Folk <folkmario@gmail.com>
2026-01-16 18:55:47 +0100

    IdleTap MVP

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿let points = 0;
+
+const pointsEl = document.getElementById("points");
+const tapBtn = document.getElementById("tapBtn");
+
+tapBtn.onclick = () => {
+  points++;
+  pointsEl.textContent = points;
+};
