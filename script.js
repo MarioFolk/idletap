d3dc4bba05a664ca35e5455f44608b9c75357f38
Mario Folk <folkmario@gmail.com>
2026-01-18 01:45:03 +0100

    Add tap functionality

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-﻿// load saved state
-let points = Number(localStorage.getItem("points")) || 0;
+﻿let points = 0;
 
 const pointsEl = document.getElementById("points");
 const tapBtn = document.getElementById("tapBtn");
 
-// update UI
-function update() {
-  pointsEl.textContent = points;
-}
-
-// tap button
 tapBtn.addEventListener("click", () => {
   points++;
-  localStorage.setItem("points", points);
-  update();
+  pointsEl.textContent = points;
 });
-
-// init
-update();
