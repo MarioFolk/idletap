d32ddf73a34c96fbc479f0a5dd4edf2dca73ed81
Mario Folk <folkmario@gmail.com>
2026-01-18 02:10:45 +0100

    Add idle income

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 ﻿let points = parseInt(localStorage.getItem("points")) || 0;
+let incomePerSecond = parseInt(localStorage.getItem("income")) || 1;
 
 const pointsEl = document.getElementById("points");
 const tapBtn = document.getElementById("tapBtn");
 
 pointsEl.textContent = points;
 
+// klik
 tapBtn.addEventListener("click", () => {
   points++;
+  update();
+});
+
+// idle income – každú sekundu
+setInterval(() => {
+  points += incomePerSecond;
+  update();
+}, 1000);
+
+function update() {
   pointsEl.textContent = points;
   localStorage.setItem("points", points);
-});
+  localStorage.setItem("income", incomePerSecond);
+}
