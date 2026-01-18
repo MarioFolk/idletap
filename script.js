5c6220971c2a78d1beaee3329d22a815b7e40544
Mario Folk <folkmario@gmail.com>
2026-01-18 02:08:00 +0100

    Save points to localStorage

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-﻿let points = 0;
+﻿let points = parseInt(localStorage.getItem("points")) || 0;
 
 const pointsEl = document.getElementById("points");
 const tapBtn = document.getElementById("tapBtn");
 
+pointsEl.textContent = points;
+
 tapBtn.addEventListener("click", () => {
   points++;
   pointsEl.textContent = points;
+  localStorage.setItem("points", points);
 });
